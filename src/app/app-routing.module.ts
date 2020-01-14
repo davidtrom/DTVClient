d3473840286454afe1ce342a4da82709e65655b6
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2020-01-14 17:39:07 -0500

    restructure for client and admin views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { RegisterUserComponent } from 'src/components/register-user/register-user.component';
-import { LoginComponent } from 'src/components/login/login.component';
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
-
-
-const routes: Routes = [
-  { path: '', component: AppComponent },
-=======
 import { RegisterUserComponent } from 'src/app/clientView/components/register-user/register-user.component';
 import { LoginComponent } from 'src/app/adminView/components/login/login.component';
 import { HomeViewComponent } from './clientView/home-view/home-view.component';
@@ -18,7 +8,6 @@ import { HomeViewComponent } from './clientView/home-view/home-view.component';
 const routes: Routes = [
   { path: '', component: HomeViewComponent},
   { path: 'login', component: LoginComponent},
->>>>>>> ba365c65299498aed0299da0f0f1a8197ef3cd65
   { path: 'register', component: RegisterUserComponent },
   
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { ReportAConcernComponent } from '../components/report-a-concern/report-a-concern.component';
-import { ReportedConcernsDisplayComponent } from '../components/reported-concerns-display/reported-concerns-display.component';
-import { RegisterUserComponent } from '../components/register-user/register-user.component';
-import { LoginComponent } from '../components/login/login.component';
-=======
 import { ReportAConcernComponent } from './clientView/components/report-a-concern/report-a-concern.component';
 import { ReportedConcernsDisplayComponent } from './clientView/components/reported-concerns-display/reported-concerns-display.component';
-import { CommunicationService } from './services/communication.service';
 import { RegisterUserComponent } from './clientView/components/register-user/register-user.component';
 import { LoginComponent } from './adminView/components/login/login.component';
 import { FooterComponent } from './clientView/layout/footer/footer.component';
 import { HomeViewComponent } from './clientView/home-view/home-view.component';
->>>>>>> ba365c65299498aed0299da0f0f1a8197ef3cd65
 
 
 @NgModule({
