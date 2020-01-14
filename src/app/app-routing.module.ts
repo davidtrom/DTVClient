b93e152d99694f3d64306b350a177516667ecdd4
chung <chung305>
2020-01-14 16:51:30 -0500

    change file structure and added clientview and adminview routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RegisterUserComponent } from 'src/components/register-user/register-user.component';
-import { LoginComponent } from 'src/components/login/login.component';
+import { RegisterUserComponent } from 'src/app/clientView/components/register-user/register-user.component';
+import { LoginComponent } from 'src/app/adminView/components/login/login.component';
+import { HomeViewComponent } from './clientView/home-view/home-view.component';
 
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: HomeViewComponent},
+  { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterUserComponent },
+  
 ];
 
 @NgModule({
