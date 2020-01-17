312b780a482d98d032e32e57008d320d13d0cd73
Kievina <56592501+Kievina@users.noreply.github.com>
2020-01-17 13:52:00 -0500

    delete inactive components in  app module and app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RequestAmbassadorComponent } from './clientView/components/request-ambassador/request-ambassador.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { UserService } from './services/user.service';
-import { AmbassadorRequestComponent } from './clientView/components/ambassador-request/ambassador-request.component';
 
 @NgModule({
   declarations: [
@@ -26,8 +25,7 @@ import { AmbassadorRequestComponent } from './clientView/components/ambassador-r
     FooterComponent,
     HomeViewComponent,
     HeaderComponent,
-    RequestAmbassadorComponent,
-    AmbassadorRequestComponent
+    RequestAmbassadorComponent
   ],
   imports: [
     BrowserModule,
