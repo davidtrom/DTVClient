62cb031623bb437f44e5fd8f27e664154eead72d
David Trombello <dtrombello@gmail.com>
2020-02-05 20:24:08 -0500

    getting to run

---
+++ src/app/clientView/components/closed-requests-display/closed-requests-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-closed-requests-display',
+  templateUrl: './closed-requests-display.component.html',
+  styleUrls: ['./closed-requests-display.component.css']
+})
+export class ClosedRequestsDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
