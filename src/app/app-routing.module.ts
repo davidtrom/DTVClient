a524e8bbb2de5221e252b7287545476702b6760c
David Trombello <dtrombello@gmail.com>
2020-02-06 00:31:16 -0500

    getting routing and displays set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ import { RequestAmbassadorComponent } from './clientView/components/request-amba
 import { ClientHomeComponent } from './clientView/components/client-home/client-home.component';
 import { RequestAmbassadorDisplayComponent } from './clientView/components/request-ambassador-display/request-ambassador-display.component';
 import { AdminHomeComponent } from './adminView/components/admin-home/admin-home.component';
+import { ClosedRequestsComponent } from './adminView/components/closed-requests/closed-requests.component';
+import { ClosedReportsComponent } from './adminView/components/closed-reports/closed-reports.component';
+import { EditRequestsComponent } from './adminView/components/edit-requests/edit-requests.component';
+import { EditReportsComponent } from './adminView/components/edit-reports/edit-reports.component';
 
 
 
@@ -25,6 +29,10 @@ const routes: Routes = [
   { path: 'client-home', component: ClientHomeComponent},
   { path: 'admin/ambassador-requests', component: RequestAmbassadorDisplayComponent},
   { path: 'admin', component: AdminHomeComponent},
+  { path: 'admin/ambassador-requests/closed', component: ClosedRequestsComponent},
+  { path: 'admin/reports-display/closed', component: ClosedReportsComponent},
+  { path: 'admin/ambassador-requests/edit', component: EditRequestsComponent},
+  { path: 'admin/reports-display/edit', component: EditReportsComponent},
 
     // if no route, redirect to home
     { path: '**', redirectTo: '' }
