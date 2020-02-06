1ec4246004711e3e49796a0fb7834c4d1525db78
David Trombello <dtrombello@gmail.com>
2020-02-06 17:28:58 -0500

    update displays and editing

--- src/app/adminView/components/closed-requests/closed-requests.component.ts
+++ src/app/adminView/components/closed-requests/closed-requests.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AmbassadorRequestService } from 'src/app/services/ambassador-request.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-closed-requests',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./closed-requests.component.css']
 })
 export class ClosedRequestsComponent implements OnInit {
+  closedAmbassadorRequests: any[];
 
-  constructor() { }
+  constructor(private ambassadorRequestService: AmbassadorRequestService, private router: Router) {
+    this.getAllClosedAmbassadorRequests();
+   }
 
   ngOnInit() {
+  }
+
+  getAllClosedAmbassadorRequests(){
+    this.ambassadorRequestService.getAllClosedRequests().subscribe(data => {this.closedAmbassadorRequests = data});
   }
 
 }
--- src/app/clientView/components/request-ambassador-display/request-ambassador-display.component.ts
+++ src/app/clientView/components/request-ambassador-display/request-ambassador-display.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AmbassadorRequestService } from 'src/app/services/ambassador-request.service';
+import { AmbassadorRequest } from 'src/app/models/ambassador-request';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-request-ambassador-display',
@@ -7,13 +9,21 @@ import { AmbassadorRequestService } from 'src/app/services/ambassador-request.se
   styleUrls: ['./request-ambassador-display.component.css']
 })
 export class RequestAmbassadorDisplayComponent implements OnInit {
+  openAmbassadorRequests: any[];
   ambassadorRequests: any[];
+  requestToClose: AmbassadorRequest;
+  requestToEdit: AmbassadorRequest;
 
-  constructor(private ambassadorRequestService : AmbassadorRequestService) {
-    this.getAmbassadorRequests();
+  constructor(private ambassadorRequestService : AmbassadorRequestService, private router: Router) {
+    this.getOpenAmbassadorRequests();
    }
 
   ngOnInit() {
+  }
+
+  getOpenAmbassadorRequests() {
+    this.ambassadorRequestService.getAllOpenRequests().subscribe(data => {this.openAmbassadorRequests = data});
+    console.log(this.ambassadorRequests);
   }
 
   getAmbassadorRequests() {
@@ -25,6 +35,11 @@ export class RequestAmbassadorDisplayComponent implements OnInit {
     this.ambassadorRequestService.close(id).subscribe(data => {this.requestToClose = data});
     console.log(this.ambassadorRequests);
     location.reload();
+  }
+
+  edit(id: number) {
+    this.ambassadorRequestService.getRequestById(id).subscribe(data => {this.requestToEdit = data});
+    this.router.navigate(['/admin/ambassador-requests/edit']);
   }
 
 
